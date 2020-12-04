077c796b2399304257611e99d8509737c245932a
hnifrma <hnifrma@gmail.com>
2020-12-04 08:34:26 +0800

    feat: add seeder

---
+++ database/seeds/FlowerCategorySeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\FlowerCategory;
+use Carbon\Carbon;
+
+
+class FlowerCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        FlowerCategory::insert([
+            [
+                'category_name' => 'Wedding Bouquet',
+                'category_img' => 'assets/categories/WeddingBouquet.jpg',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'category_name' => 'Standing Flower',
+                'category_img' => 'assets/categories/StandingFlower.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'category_name' => 'Hand Bouquet',
+                'category_img' => 'assets/categories/HandBouquet.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'category_name' => 'Basket Flower',
+                'category_img' => 'assets/categories/BasketFlower.jpg',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+        ]);
+    }
+}
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +13,29 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::insert([
+            [
+                'role_id'=>'1',
+                'username'=>'Big Bezzos',
+                'email'=>'big@big.com',
+                'address'=>'Jalan dimana mana depan syahdan enal bous',
+                'gender'=>'male',
+                'dob'=>Carbon::today(),
+                'password'=>Hash::make('123123123'),
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'role_id'=>'2',
+                'username'=>'User Sedgirl',
+                'email'=>'asd@asd.com',
+                'address'=>'Jalan dimana mana depan syahdan enal bous',
+                'gender'=>'female',
+                'dob'=>Carbon::today(),
+                'password'=>Hash::make('123123123'),
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+        ]);
     }
 }
